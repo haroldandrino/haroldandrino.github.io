3b45de574fb08f1080d5511cdcf04287c6e07db1
Harold Andrino <harold@webfx.com>
2023-04-03 23:18:31 +0800

    Growmodo Todo App

---
+++ src/pages/HelpPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae commodi
+      nam rerum debitis sapiente provident id consequuntur expedita quia dolores
+      facere cum cumque, praesentium perspiciatis necessitatibus tempore
+      recusandae deserunt tempora?
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae commodi
+      nam rerum debitis sapiente provident id consequuntur expedita quia dolores
+      facere cum cumque, praesentium perspiciatis necessitatibus tempore
+      recusandae deserunt tempora?
+    </p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
